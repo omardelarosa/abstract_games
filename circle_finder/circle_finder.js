e5e201e885c64f76202a87ac30dea7f509ac5a12
omar delarosa <thedelarosa@gmail.com>
2013-10-13 16:02:08 -0400

    added instructions, burning action and other minor tweaks

--- circle_finder/circle_finder.js
+++ circle_finder/circle_finder.js
@@ -2,6 +2,29 @@ var app = {
 
 	name: "circle.finder",
 
+	key: {
+		bindings: [ 
+						"space",
+						"o",
+						"b",
+						"up",
+						"down",
+						"left",
+						"right",
+						"click"
+					],
+		descriptions: [
+							"shoot circles", 
+							"toggle options/instructions", 
+							"burn with rage",
+							"move your avatar up",
+							"move your avatar down",
+							"move your avatar left",
+							"move your avatar right", 
+							"teleport your avatar to a clicked point"
+						]
+	},
+
 	initialize: function(){
 		
 		//set name of game at the top.
@@ -13,7 +36,7 @@ var app = {
 
 		//options
 		app.movement.rate = 2;
-		app.movement.step = 3;
+		app.movement.step = 10;
 
 		app.board.$el = app.board.$el || $('#game_board');
 		app.board.start_size = (function(){
@@ -46,14 +69,46 @@ var app = {
 						 .attr("stroke-width",app.player.start_stroke_width)
 
 		
+		//event bindings
 
 		$(document).on('keydown',app.keydown_callback);
+
+		$(document).on('click',function(e){
+			app.movement.teleport(app.player.avatar,e);
+		});
 
 		//make a bunch of circles
 		for(var i = 0; i < app.board.start_size; i++) {
 			app.action.shoot();
 			$('#current_score').text("0");
 			$('#max_score').text("/ "+(i+1));
+		}
+
+		//
+		app.render.instructions();
+
+	},
+	render: {
+		instructions: function(){
+			var bindings = app.key.bindings.length,
+				i = 0,
+				$instructions = $('#instructions'),
+				$heading = $('<h2 id="instructions_heading">instructions</h2>');
+				$body = $('<ul id="instructions_list"></ul>');
+
+
+			while(i < bindings) {
+				var $li = $("<li class='key_info'></li>"),
+					$span1 = $("<span class='key_name'>"+app.key.bindings[i]+":</span>"),
+					$span2 = $("<span class='key_desc'>\t"+app.key.descriptions[i]+"</span>");
+				$li.append($span1)
+				$li.append($span2)
+				$body.append($li)
+
+				i++
+			}
+
+				$instructions.append($heading).append($body);
 		}
 	},
 	board: {
@@ -87,9 +142,23 @@ var app = {
 
 		},
 		updateScore: function(){
-			app.board.score = app.collisions.getTotal()
+			app.board.score = app.collisions.getTotal();
+			var max = app.board.size();
 			$('#current_score').text(app.board.score)
-			$('#max_score').text(" / "+app.board.size())
+			$('#max_score').text(" / "+max)
+
+			//change bg color to player's color if they win.
+			if(app.board.checkWin()){
+				app.board.$el.css({backgroundColor: app.player.start_inner_color })
+			}
+
+		},
+		checkWin: function(){
+			if ( app.board.score === app.board.size() ) {
+				return true;
+			} else {
+				return false;
+			}
 		},
 		changeColors: function(score){
 			switch (score){
@@ -127,6 +196,10 @@ var app = {
 
 	},
 	action: {
+		burn: function(e) {
+			e.preventDefault();
+			app.player.avatar.glow(20);
+		},
 		toggleOptions: function(e){
 			e.preventDefault();
 			var $button = $('#more_button'),
@@ -212,7 +285,34 @@ var app = {
 	},
 	movement: {
 		//movement steps/rates
-		
+		teleport: function(target, e){
+			e.preventDefault();
+
+			// console.log(e, target);
+
+			//adjusted for navbar size
+
+			var old_pos = app.player.getCurrentPosition(),
+				new_x = e.clientX,
+				new_y = parseInt((e.clientY*0.9));
+
+			var destination = {
+				cx: new_x,
+				cy: new_y
+			}
+
+			var points_data = aux.getTwoPointsData(old_pos.x,new_x,old_pos.y,new_y)
+
+			// distance / ms
+			var time = parseInt((points_data.distance) / 2)*5;
+
+			target.hide
+
+			target.animate(destination, time, function(){
+				var pos = app.player.getCurrentPosition()
+				app.action.collide(pos);
+			})
+		},
 		//Targets are Raphael Elements
 		slide: function(target, direction_name, e){
 			
@@ -252,6 +352,17 @@ var app = {
 					break;
 			}
 
+			// check if destination is off the map
+			if ( destination.cx < 0 ) {
+				destination.cx = app.board.width;
+			} else if ( destination.cx > app.board.width ) {
+				destination.cx = 0;
+			} else if ( destination.cy < 0 ) {
+				destination.cy = app.board.height;
+			} else if ( destination.cy > app.board.height ) {
+				destination.cy = 0;
+			}
+
 			target.animate(destination, app.movement.rate, function(){
 				var pos = app.player.getCurrentPosition()
 				app.action.collide(pos);
@@ -262,10 +373,9 @@ var app = {
 	keydown_callback: function(e){
 
 			var key_num = e.keyCode,
-				key_name = aux.key_to_s(key_num),
-				key_bindings = ["space","o","up","down","left","right"];
+				key_name = aux.key_to_s(key_num);
 
-			var command_index = _.indexOf(key_bindings,key_name);
+			var command_index = _.indexOf(app.key.bindings,key_name);
 
 			switch (command_index) {
 				case -1:
@@ -276,6 +386,10 @@ var app = {
 					break;
 				case 1:
 					app.action.toggleOptions(e)
+					break;
+				case 2:
+					app.action.burn(e)
+					break;
 				default:
 					app.movement.slide(app.player.avatar, key_name, e)
 					break;
@@ -299,6 +413,8 @@ var aux = {
 				return "space"
 			case 79:
 				return "o"
+			case 66:
+				return "b"
 		}
 	},
 	get_random_x: function(){
@@ -306,6 +422,14 @@ var aux = {
 	},
 	get_random_y: function(){
 		return parseInt( ( Math.random()*app.board.height) + 1 );
+	},
+	getTwoPointsData: function(x1,x2,y1,y2){
+		var data = {}
+		data.slope = (y2 - y1) / (x2 - x1);
+		data.distance = Math.sqrt( Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2) )
+		data.slope_int = parseInt(data.slope);
+		data.distance_int = parseInt(data.distance);
+		return data;
 	}
 }
 
